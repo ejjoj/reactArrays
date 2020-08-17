4eb8849d3144cd6463e9cb317cee7bddc1c8a95c
ejjoj <szymaskibartosz@gmail.com>
2020-08-17 18:53:28 +0200

    Managing data in object.

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
-const Item = (props) => <li>{`owoc ${props.content}`}</li>;
+const data = {
+  users: [
+    {
+      id: 1,
+      age: 29,
+      name: 'Arek'
+    },
+    {
+      id: 2,
+      age: 49,
+      name: 'Marta'
+    },
+    {
+      id: 3,
+      age: 19,
+      name: 'Stasia'
+    },
+    {
+      id: 4,
+      age: 24,
+      name: 'Karol'
+    }
+  ]
+}
+
+const Item = ({user}) => (
+  <div>
+    <h1>Użytkownik {user.name}</h1>
+    <h2>Ma {user.age} lat</h2>
+  </div>
+);
 
 class ListItems extends React.Component {
-  state = {
-    items: ['jabłko', 'śliwka', 'gruszka']
-  }
 
   render() {
-    const Items = this.state.items.map((item) => <Item key={item} content={item} />);
+    const users = this.props.data.users;
+    const Items = users.map((user) => <Item key={user.id} user={user} />);
     return (
       <ul>
         {Items}
@@ -15,4 +43,4 @@ class ListItems extends React.Component {
   }
 }
 
-ReactDOM.render(<ListItems />, document.getElementById('root'));
+ReactDOM.render(<ListItems data={data} />, document.getElementById('root'));
