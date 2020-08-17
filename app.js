57dd02c9865582151b7d2da203acefae856dee1f
ejjoj <szymaskibartosz@gmail.com>
2020-08-17 18:37:32 +0200

    Learned how to use .map() with React.js

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-class ListItems extends React.Component {}
+class ListItems extends React.Component {
+  state = {
+    items: ['jabłko', 'śliwka', 'gruszka']
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.items.map((item) => <li key={item}>{`owoc ${item}`}</li>)}
+      </ul>
+    );
+  }
+}
 
 ReactDOM.render(<ListItems />, document.getElementById('root'));
