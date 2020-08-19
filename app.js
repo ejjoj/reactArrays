51cc2a3eddf23e5009b27a5faa46319ef8c2ce42
ejjoj <szymaskibartosz@gmail.com>
2020-08-19 18:16:41 +0200

    usage of array.filter explained

--- app.js
+++ app.js
@@ -3,37 +3,45 @@ const data = {
     {
       id: 1,
       age: 29,
-      name: 'Arek'
+      name: 'Arek',
+      sex: 'male'
     },
     {
       id: 2,
-      age: 49,
-      name: 'Marta'
+      age: 20,
+      name: 'Klodiś',
+      sex: 'female'
     },
     {
       id: 3,
       age: 19,
-      name: 'Stasia'
+      name: 'Stasia',
+      sex: 'female'
     },
     {
       id: 4,
       age: 24,
-      name: 'Karol'
+      name: 'Karol',
+      sex: 'male'
     }
   ]
 }
 
 const Item = ({user}) => (
-  <div>
-    <h1>Użytkownik {user.name}</h1>
-    <h2>Ma {user.age} lat</h2>
+  <div className='userInfo'>
+    <h1>{user.name}</h1>
+    <p>Informacje o użytkowniku</p>
+    <p>Wiek użytkownika: <strong>{user.age}</strong></p>
+    <p>Płeć użytkowika: {user.sex}</p>
   </div>
 );
 
 class ListItems extends React.Component {
+    state = {}
 
   render() {
-    const users = this.props.data.users;
+    let users = this.props.data.users;
+    users = users.filter((user) => user.sex === 'male');
     const Items = users.map((user) => <Item key={user.id} user={user} />);
     return (
       <ul>
